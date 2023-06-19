ef54cb34a4dbbfa63ad6015af0e8674e771cb044
rabia aksamaz <aksamazrabia@gmail.com>
2023-06-19 14:14:16 +0300

    ders-1 eklendi

---
+++ ders-1/js/01-hello-world.js
@@ -0,0 +1,10 @@
+//************Merhaba Dünya Örnekleri*********** */
+
+//Dökümana(document) Yazdırma İşlemi ile Merhaba Dünya:
+
+    //document.write('Merhaba Dünya');
+
+//Uyarı Mesajı(alert) ile Merhaba Dünya: 
+    //alert('Hello World');
+//console.log ile Merhaba Dünya:
+    console.log("Merhaba Dünya")
