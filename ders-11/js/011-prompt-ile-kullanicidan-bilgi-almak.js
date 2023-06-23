7064e9218fc469f5911a3749963cada985ad191c
rabia aksamaz <aksamazrabia@gmail.com>
2023-06-23 11:40:01 +0300

    ders-11 bitti

---
+++ ders-11/js/011-prompt-ile-kullanicidan-bilgi-almak.js
@@ -0,0 +1,11 @@
+//***************prompt ile Kullanıcıdan Bilgi Almak*****************
+
+//https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
+
+
+let fullName=prompt("Lütfen adınızı giriniz:")
+
+
+let greeting=document.querySelector("#greeting")
+
+greeting.innerHTML=`${greeting.innerHTML} <small style="color:red">${fullName}</small>`
