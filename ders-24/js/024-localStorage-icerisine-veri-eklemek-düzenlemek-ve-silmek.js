5993a7ab18597db63ba1210e0d772496586104f5
rabia aksamaz <aksamazrabia@gmail.com>
2023-06-27 10:10:39 +0300

    ders 24-25 bitti

---
+++ ders-24/js/024-localStorage-icerisine-veri-eklemek-düzenlemek-ve-silmek.js
@@ -0,0 +1,13 @@
+//https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+
+
+/*
+localStorage.getItem('myCat')
+null
+localStorage.setItem('myCat','KodKedisi');
+undefined
+localStorage.getItem('myCat');
+'KodKedisi'
+localStorage.removeItem('myCat')
+
+*/
